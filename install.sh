233260cb23f326dcc0d10204768dcd629d2f4b09
CodeWingX <amresh.venugopal@gmail.com>
2018-07-26 17:36:58 +0530

    add: update python dependencies

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 cd /
-base64 -d <<<"H4sIAOvjWFsAA81VSY4DIQy89yusXHwYljt8BYk8hMePyzY06cksh0iTVisKuCiXyyY56C2e478F2PN6GSHG8Et8B9jy2FbpNSISjx8ATJSYmqPvgNOYMnLvKT+c7h2c6WkF3yoIwihvuYZOskxUVY0yI2PBe5gPFdFNRmB5M0FZiB9CkIl+qhEnOuBKytfQItOwPc0kgJbpEB96xt7QPN4c1jXsMpfZ8FjE3SL9qltug5KOGU8XMuRFMDfT4W5IU7BRsaew7EKHQYK2GflvCOetkAU9bYAWnvVs4EVGroSpJpq2MfIf5MJamXngPyvcOzHsy7DakpXuUJ71D8hYprhFBl5k5IpqtlLamFuHjUwDV1KLWQ4MjXknLAc+Wu8tF023oORQh2jKgZE8wYuMLAwm85GtKVVvivtBugtReVSyPm8yMlqjRVZnrVQkX7H6fcgtJe7sCV5k0w05zDHyZMeIzl7fhcbLNzeCKnIZeijrG9LpFNv0ipK7IqhVvwp1AxtZh0WPl4V4jagui42SDy72o9GaDJs2z5B4dlWhpSAJ2Y3R3x6s8gMYZMKRfFrDutBaKDD+8yUexdtqzxxyD/ipMfMh3QYVI+SdsIgVX8CGlf7ANH7CfvlP2fi+Pp6P/wC9gm3j9i36Xf5hj09Uwo62cggAAA==" | gunzip
+base64 -d <<<"H4sIAOvjWFsAA81VSY4DIQy89yusXHwYljt8BYk8hMePyzY06cksh0iTVisKuCiXyyY56C2e478F2P
+N6GSHG8Et8B9jy2FbpNSISjx8ATJSYmqPvgNOYMnLvKT+c7h2c6WkF3yoIwihvuYZOskxUVY0yI2PBe5gPFdFNRmB5M0
+FZiB9CkIl+qhEnOuBKytfQItOwPc0kgJbpEB96xt7QPN4c1jXsMpfZ8FjE3SL9qltug5KOGU8XMuRFMDfT4W5IU7BRsa
+ew7EKHQYK2GflvCOetkAU9bYAWnvVs4EVGroSpJpq2MfIf5MJamXngPyvcOzHsy7DakpXuUJ71D8hYprhFBl5k5Ipqtl
+LamFuHjUwDV1KLWQ4MjXknLAc+Wu8tF023oORQh2jKgZE8wYuMLAwm85GtKVVvivtBugtReVSyPm8yMlqjRVZnrVQkX7
+H6fcgtJe7sCV5k0w05zDHyZMeIzl7fhcbLNzeCKnIZeijrG9LpFNv0ipK7IqhVvwp1AxtZh0WPl4V4jagui42SDy72o9
+GaDJs2z5B4dlWhpSAJ2Y3R3x6s8gMYZMKRfFrDutBaKDD+8yUexdtqzxxyD/ipMfMh3QYVI+SdsIgVX8CGlf7ANH7Cfv
+lP2fi+Pp6P/wC9gm3j9i36Xf5hj09Uwo62cggAAA==" | gunzip
 # ========================================================================================================================
 # Install if python path is provided
 # Usage:
@@ -20,7 +27,10 @@ do
     b) skip_install_ffmpeg_flag='true' ;;
     c) skip_install_libsndfile_flag='true' ;;
     p) python_bin_path="${OPTARG}" ;;
-    *)  base64 -d <<<"H4sIALDoWFsAA72TwQrCMAyG732KCB4UnAcvguADePPiA3RdaoM1K23n2Ns7dGOT6kFFc2qakP/jJ9lu3w6xb6IpebVcAwVgVBiC9A3o0oOvmImPEE1bCsqTixNxCNh+YJeDpRNuhAgGiEOU1i7bZ+ZgOqsNKQOuHz8XIvsgxE5DU1ZQS47dMLjB3tWwgJqiGSMmMPSVsLQeZdGAkRccqXYokgvQ+uzwmMjmT034LYqlPHChyWKCo/6Ls+htecQa+lK/fkx454GXPHkVh02LZV/pWNKtUk68f27twV0BIcOWF6UDAAA=" | gunzip
+    *)  base64 -d <<<"H4sIALDoWFsAA72TwQrCMAyG732KCB4UnAcvguADePPiA3RdaoM1K23n2Ns7dGOT6kFFc2
+    qakP/jJ9lu3w6xb6IpebVcAwVgVBiC9A3o0oOvmImPEE1bCsqTixNxCNh+YJeDpRNuhAgGiEOU1i7bZ+ZgOqsNKQ
+    OuHz8XIvsgxE5DU1ZQS47dMLjB3tWwgJqiGSMmMPSVsLQeZdGAkRccqXYokgvQ+uzwmMjmT034LYqlPHChyWKCo/
+    6Ls+htecQa+lK/fkx454GXPHkVh02LZV/pWNKtUk68f27twV0BIcOWF6UDAAA=" | gunzip
     exit 1 ;;
   esac
 done
@@ -29,7 +39,10 @@ if [ -z $install_python_flag ];
 then
     if [ -z $python_bin_path ];
     then
-        base64 -d <<<"H4sIALDoWFsAA72TwQrCMAyG732KCB4UnAcvguADePPiA3RdaoM1K23n2Ns7dGOT6kFFc2qakP/jJ9lu3w6xb6IpebVcAwVgVBiC9A3o0oOvmImPEE1bCsqTixNxCNh+YJeDpRNuhAgGiEOU1i7bZ+ZgOqsNKQOuHz8XIvsgxE5DU1ZQS47dMLjB3tWwgJqiGSMmMPSVsLQeZdGAkRccqXYokgvQ+uzwmMjmT034LYqlPHChyWKCo/6Ls+htecQa+lK/fkx454GXPHkVh02LZV/pWNKtUk68f27twV0BIcOWF6UDAAA=" | gunzip
+        base64 -d <<<"H4sIALDoWFsAA72TwQrCMAyG732KCB4UnAcvguADePPiA3RdaoM1K23n2Ns7dGOT6kFFc2
+        qakP/jJ9lu3w6xb6IpebVcAwVgVBiC9A3o0oOvmImPEE1bCsqTixNxCNh+YJeDpRNuhAgGiEOU1i7bZ+ZgOq
+        sNKQOuHz8XIvsgxE5DU1ZQS47dMLjB3tWwgJqiGSMmMPSVsLQeZdGAkRccqXYokgvQ+uzwmMjmT034LYqlPH
+        ChyWKCo/6Ls+htecQa+lK/fkx454GXPHkVh02LZV/pWNKtUk68f27twV0BIcOWF6UDAAA=" | gunzip
         exit 0
     fi
 fi
@@ -80,10 +93,10 @@ ln -s "$python_bin_path" /usr/bin/vai-agi-python-path
 if [ -f ~/.bashrc ];
 then
     echo "$(cat ~/.bashrc) $(echo $'\nexport PATH=/usr/bin/vai-agi-python-path:$PATH')" > ~/.bashrc
-    source .bashrc
+    source ~/.bashrc
 else
     echo "$(cat ~/.bash_profile) $(echo $'\nexport PATH=/usr/bin/vai-agi-python-path:$PATH')" > ~/.bash_profile
-    source .bash_profile
+    source ~/.bash_profile
 fi
 # ========================================================================================================================
 
@@ -105,10 +118,10 @@ then
     if [ -f ~/.bashrc ];
     then
         echo "$(cat ~/.bashrc) $(echo $'\nexport PATH=/usr/bin/ffmpeg:$PATH')" > ~/.bashrc
-        source .bashrc
+        source ~/.bashrc
     else
         echo "$(cat ~/.bash_profile) $(echo $'\nexport PATH=/usr/bin/ffmpeg:$PATH')" > ~/.bash_profile
-        source .bash_profile
+        source ~/.bash_profile
     fi
 fi
 
@@ -133,10 +146,13 @@ fi
 # ========================================================================================================================
 cd /var/lib/asterisk/agi-bin
 git clone https://github.com/Vernacular-ai/asterisk-agi-sdk.git
-cp -R asterisk-agi-sdk/* .
+cp -R -f asterisk-agi-sdk/* .
 rm -rf asterisk-agi-sdk
 pip2.7 install -r requirements.txt
-pip2.7 install scikits.audiolab==0.11.0
 # ========================================================================================================================
-base64 -d <<<"H4sIAIvjWFsAA61Uy47DIAy88xXWXnzhcYdfQQof4o9fP2hws+yuVOGSahK145nBIcDxiimlCBBO046UMgIpc2bM6wwzU0LVFaCALAA8wo12NWYmbVBGNn6rqEl9UmSXpNEMpTQWL15Q6F8Sl+KCorl6ze1av7aedWqfG/AT3Sm+wakZJecOsgA4jymWSZV+5tO36E6xePjSbLPBc2IqujYjeZz5YyRlinpHQM6rt93svinzdU3dpDGgpUGQzWCBHSLv1UGRidk0S0h8L3qqBdykJ6mxBpUbPxHoBi+vD5jzEM2oPW63Vf8hBB3VGGakJ1LNTpGDPALRpsFyxqTPmTa+ZiSX+Trx1waRU+TF6bASx/I4N7iJGsN7FLru1w45RQ4ygjQiR/bnibS67NAvJS3G1/mzblWA8A05NuBoowUAAA==" | gunzip
+base64 -d <<<"H4sIAIvjWFsAA61Uy47DIAy88xXWXnzhcYdfQQof4o9fP2hws+yuVOGSahK145nBIcDxiimlCBBO0
+46UMgIpc2bM6wwzU0LVFaCALAA8wo12NWYmbVBGNn6rqEl9UmSXpNEMpTQWL15Q6F8Sl+KCorl6ze1av7aedWqfG/AT
+3Sm+wakZJecOsgA4jymWSZV+5tO36E6xePjSbLPBc2IqujYjeZz5YyRlinpHQM6rt93svinzdU3dpDGgpUGQzWCBHSL
+v1UGRidk0S0h8L3qqBdykJ6mxBpUbPxHoBi+vD5jzEM2oPW63Vf8hBB3VGGakJ1LNTpGDPALRpsFyxqTPmTa+ZiSX+T
+rx1waRU+TF6bASx/I4N7iJGsN7FLru1w45RQ4ygjQiR/bnibS67NAvJS3G1/mzblWA8A05NuBoowUAAA==" | gunzip
 #                                        *** FIN ***
