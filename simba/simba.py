cc405fc5eaa17dafc9b470fe38f3bfcf213fec05
adrianhumphrey111 <adrian.humphrey@tealium.com>
2018-08-22 19:39:30 -0700

    Fixed import.

--- simba/simba.py
+++ simba/simba.py
@@ -3,7 +3,7 @@ from utils.log import __console
 
 class Simba:
     def __init__(self):
-        r = requests.get("172.31.16.165/initiatecall")
+        r = requests.get("http://172.31.16.165/initiatecall")
         __console.log("This is the response!")
         __console.log(r)
 
