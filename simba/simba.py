8d11cd2ed33ba948e58bb93f44fd48329c8723df
adrianhumphrey111 <adrian.humphrey@tealium.com>
2018-08-22 19:33:00 -0700

    Added console log to Simba.

--- simba/simba.py
+++ simba/simba.py
@@ -1,8 +1,10 @@
 import requests
+from utils.log import __console
+
 
 class Simba:
     def __init__():
         r = requests.get("172.31.16.165/initiatecall")
-        print("This is the response!")
-        print(r)
+        __console.log("This is the response!")
+        __console.log(r)
 
